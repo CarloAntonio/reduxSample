7da116f52c976c1967695b30e9ac6f6c1e52577e
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-11 09:42:36 -0700

    chore: split reducer into multiple reducers, combine programmatically

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,9 +1,7 @@
-
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
-    counter: 0,
-    results: []
+    counter: 0
 }
 
 const reducer = (state = initialState, action) => {
@@ -30,27 +28,6 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 counter: state.counter - action.val
-            }
-        case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({
-                    id: new Date(), 
-                    value: state.counter
-                }) //concat create a new array
-            }
-        case actionTypes.DELETE_RESULT:
-            //option 1
-            // const id = 2;
-            // const newArray = [...state.results]; //create a copy of old array
-            // newArray.splice(id, 1);
-
-            //option 2
-            const updatedArray = state.results.filter(result => result.id !== action.resultElementId); //filter returns new array
-
-            return {
-                ...state,
-                results: updatedArray
             }
     }
 
---
+++ src/store/reducers/result.js
@@ -0,0 +1,37 @@
+
+import * as actionTypes from '../actions';
+
+const initialState = {
+    results: []
+}
+
+const reducer = (state = initialState, action) => {
+
+    switch(action.type) {
+        case actionTypes.STORE_RESULT:
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(), 
+                    value: action.result
+                }) //concat create a new array
+            }
+        case actionTypes.DELETE_RESULT:
+            //option 1
+            // const id = 2;
+            // const newArray = [...state.results]; //create a copy of old array
+            // newArray.splice(id, 1);
+
+            //option 2
+            const updatedArray = state.results.filter(result => result.id !== action.resultElementId); //filter returns new array
+
+            return {
+                ...state,
+                results: updatedArray
+            }
+    }
+
+    return state;
+};
+
+export default reducer;
