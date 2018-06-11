0705ec5d90f241e0d32f728aedd05f6f0ddc799e
Carlo Bilbao <cbilbao88@gmail.com>
2018-06-10 17:07:01 -0700

    feat: add add and delete button functionality

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,30 +1,55 @@
+
 const initialState = {
-    counter: 0
+    counter: 0,
+    results: []
 }
 
 const reducer = (state = initialState, action) => {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1
-        }
-    }
 
-    if(action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1
-        }
-    }
+    switch(action.type) {
+        case 'INCREMENT':
+            //option 1 of changing state, immutably
+            return {
+                ...state,
+                counter: state.counter + 1
+            }
+        case 'DECREMENT':
+            //option 2 of changing state, immutably
+            const newState = Object.assign({}, state);
+            newState.counter = state.counter - 1;
+            return newState;
 
-    if(action.type === 'ADD') {
-        return {
-            counter: state.counter + action.val
-        }
-    }
+        case 'ADD':
+            return {
+                ...state,
+                counter: state.counter + action.val
+            }
+        case 'SUBTRACT':
+            return {
+                ...state,
+                counter: state.counter - action.val
+            }
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({
+                    id: new Date(), 
+                    value: state.counter
+                }) //concat create a new array
+            }
+        case 'DELETE_RESULT':
+            //option 1
+            // const id = 2;
+            // const newArray = [...state.results]; //create a copy of old array
+            // newArray.splice(id, 1);
 
-    if(action.type === 'SUBTRACT') {
-        return {
-            counter: state.counter - action.val
-        }
+            //option 2
+            const updatedArray = state.results.filter(result => result.id !== action.resultElementId); //filter returns new array
+
+            return {
+                ...state,
+                results: updatedArray
+            }
     }
 
     return state;
